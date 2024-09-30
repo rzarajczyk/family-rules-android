813b5af293aa549deed308fcb8a57d574d2ef76d
rzarajczyk <rzarajczyk@gmail.com>
2024-09-30 20:56:35 +0200

    Initial commit.

---
+++ app/src/main/java/pl/zarajczyk/familyrulesandroid/MainActivity.kt
@@ -0,0 +1,237 @@
+package pl.zarajczyk.familyrulesandroid
+
+import android.app.AppOpsManager
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.drawable.toBitmap
+import org.json.JSONArray
+import org.json.JSONObject
+import pl.zarajczyk.familyrulesandroid.ui.theme.FamilyRulesAndroidTheme
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var updateRunnable: Runnable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        if (!isUsageStatsPermissionGranted()) {
+            navigateToUsageStatsPermissionSettings()
+        }
+        setupContent()
+        setupPeriodicUpdate()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(updateRunnable)
+    }
+
+    private fun setupContent() {
+        setContent {
+            FamilyRulesAndroidTheme {
+                MainScreen(fetchUsageStats())
+            }
+        }
+    }
+
+    private fun setupPeriodicUpdate() {
+        updateRunnable = Runnable {
+            val usageStatsList = fetchUsageStats()
+            sendUsageStatsToServer(usageStatsList)
+            setupContent()
+            handler.postDelayed(updateRunnable, 5000)
+        }
+        handler.post(updateRunnable)
+    }
+
+    private fun isUsageStatsPermissionGranted(): Boolean {
+        val appOpsManager = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOpsManager.checkOpNoThrow(
+            AppOpsManager.OPSTR_GET_USAGE_STATS,
+            android.os.Process.myUid(),
+            packageName
+        )
+        return mode == AppOpsManager.MODE_ALLOWED
+    }
+
+    private fun navigateToUsageStatsPermissionSettings() {
+        val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun fetchUsageStats(): List<UsageStats> {
+        val usageStatsManager = getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val endTime = System.currentTimeMillis()
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        val startTime = calendar.timeInMillis
+        val usageStatsList = usageStatsManager.queryUsageStats(
+            UsageStatsManager.INTERVAL_DAILY,
+            startTime,
+            endTime
+        )
+        return usageStatsList.filterNot { isSystemApp(it.packageName) || it.totalTimeInForeground == 0L }
+    }
+
+    private fun isSystemApp(packageName: String): Boolean {
+        return try {
+            val packageManager = applicationContext.packageManager
+            val appInfo = packageManager.getApplicationInfo(packageName, 0)
+            (appInfo.flags and ApplicationInfo.FLAG_SYSTEM) != 0
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+    }
+
+    private fun sendUsageStatsToServer(usageStatsList: List<UsageStats>) {
+        val json = convertUsageStatsToJson(usageStatsList)
+        val url = URL("http://cloud.local:8080/api/v1/report")
+        try {
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            connection.setRequestProperty("Content-Type", "application/json; utf-8")
+            connection.doOutput = true
+            connection.outputStream.use { os: OutputStream ->
+                val input = json.toByteArray(Charsets.UTF_8)
+                os.write(input, 0, input.size)
+            }
+            connection.responseCode // To trigger the request
+        } catch (e: Exception) {
+            // Ignore any errors
+        }
+    }
+
+    private fun convertUsageStatsToJson(usageStatsList: List<UsageStats>): String {
+        val jsonArray = JSONArray()
+        usageStatsList.forEach { stat ->
+            val jsonObject = JSONObject()
+            jsonObject.put("packageName", stat.packageName)
+            jsonObject.put("lastTimeUsed", stat.lastTimeUsed)
+            jsonObject.put("totalTimeInForeground", stat.totalTimeInForeground)
+            jsonArray.put(jsonObject)
+        }
+        return jsonArray.toString()
+    }
+}
+
+@Composable
+fun MainScreen(usageStatsList: List<UsageStats>) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { AppTopBar() }
+    ) { innerPadding ->
+        UsageStatsDisplay(
+            usageStatsList = usageStatsList,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar() {
+    TopAppBar(
+        title = { Text(text = "Family Rules") },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color(0xFF000080),
+            titleContentColor = Color.White
+        )
+    )
+}
+
+@Composable
+fun UsageStatsDisplay(usageStatsList: List<UsageStats>, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val dateFormat = remember { SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()) }
+
+    LazyColumn(modifier = modifier.padding(16.dp)) {
+        items(usageStatsList) { stat ->
+            val (appName, appIcon) = getAppNameAndIcon(stat.packageName, context)
+            val lastUsed = dateFormat.format(Date(stat.lastTimeUsed))
+            val totalTimeInSeconds = stat.totalTimeInForeground / 1000
+            val hours = totalTimeInSeconds / 3600
+            val minutes = (totalTimeInSeconds % 3600) / 60
+            val totalTimeFormatted = String.format("%02d:%02d", hours, minutes)
+
+            Row(modifier = Modifier.padding(vertical = 8.dp)) {
+                appIcon?.let {
+                    Image(
+                        bitmap = it.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier.size(48.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    Text(
+                        text = appName,
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontSize = 18.sp
+                    )
+                    Text(text = "Last Used: $lastUsed", style = MaterialTheme.typography.bodyMedium)
+                    Text(
+                        text = "Total Time: $totalTimeFormatted",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
+}
+
+private fun getAppNameAndIcon(packageName: String, context: Context): Pair<String, Bitmap?> {
+    return try {
+        val packageManager = context.packageManager
+        val appInfo = packageManager.getApplicationInfo(packageName, 0)
+        val appName = packageManager.getApplicationLabel(appInfo).toString()
+        val appIcon = packageManager.getApplicationIcon(appInfo).toBitmap()
+        Pair(appName, appIcon)
+    } catch (e: PackageManager.NameNotFoundException) {
+        Pair(packageName, null)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UsageStatsDisplayPreview() {
+    FamilyRulesAndroidTheme {
+        UsageStatsDisplay(emptyList())
+    }
+}
