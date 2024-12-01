f9fedcf44a1033eb26a790435249d2a1b3d808f4
rzarajczyk <rzarajczyk@gmail.com>
2024-12-01 13:40:55 +0100

    boot

--- app/src/main/java/pl/zarajczyk/familyrulesandroid/scheduled/BootReceiver.kt
+++ app/src/main/java/pl/zarajczyk/familyrulesandroid/scheduled/BootReceiver.kt
@@ -3,17 +3,15 @@ package pl.zarajczyk.familyrulesandroid.scheduled
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import pl.zarajczyk.familyrulesandroid.gui.PermanentNotification
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            Log.i("BootReceiver", "Boot completed")
             // Start your service here
-            Intent(context, PermanentNotification::class.java).also {
+            Intent(context, KeepAliveService::class.java).also {
                 context.startService(it)
             }
         }
     }
 }
+
