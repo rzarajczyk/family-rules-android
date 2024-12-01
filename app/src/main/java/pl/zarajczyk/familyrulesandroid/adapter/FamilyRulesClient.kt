b6c6c0de7db76c9d71d35d5b820890c2b9ecb31c
rzarajczyk <rzarajczyk@gmail.com>
2024-12-01 13:22:31 +0100

    isDevMode

--- app/src/main/java/pl/zarajczyk/familyrulesandroid/adapter/FamilyRulesClient.kt
+++ app/src/main/java/pl/zarajczyk/familyrulesandroid/adapter/FamilyRulesClient.kt
@@ -1,6 +1,7 @@
 package pl.zarajczyk.familyrulesandroid.adapter
 
 import android.content.Context
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
@@ -19,7 +20,14 @@ class FamilyRulesClient(
     private val context: Context,
     private val settingsManager: SettingsManager
 ) {
-    private val isDevMode = true
+    private val isDevMode = (Build.FINGERPRINT.startsWith("generic")
+            || Build.FINGERPRINT.startsWith("unknown")
+            || Build.MODEL.contains("google_sdk")
+            || Build.MODEL.contains("Emulator")
+            || Build.MODEL.contains("Android SDK built for x86")
+            || Build.MANUFACTURER.contains("Genymotion")
+            || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+            || "google_sdk" == Build.PRODUCT)
 
 
     fun sendLaunchRequest() {
